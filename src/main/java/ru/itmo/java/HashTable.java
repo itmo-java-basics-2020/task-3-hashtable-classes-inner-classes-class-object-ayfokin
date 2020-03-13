966eab9058cea9b0109d8a7cf5b2ddfdbd98369a
ayfokin <ayfokin@mail.ru>
2020-03-13 19:28:55 +0300

    Done task

--- src/main/java/ru/itmo/java/HashTable.java
+++ src/main/java/ru/itmo/java/HashTable.java
@@ -1,23 +1,183 @@
 package ru.itmo.java;
 
-import java.util.Map;
-
 public class HashTable {
+    //Robin Hood hashing
+    private static final double DEFAULT_LOAD_FACTOR = 0.5;
 
-    Object put(Object key, Object value) {
-        throw new UnsupportedOperationException();
+    private static class Entry {
+        private Object key;
+        private Object value;
+        private int positionInArray;
+        private boolean tombstone;
+
+        private Entry(Object newKey, Object newValue, int newPositionInArray) {
+            key = newKey;
+            value = newValue;
+            positionInArray = newPositionInArray;
+            tombstone = false;
+        }
     }
 
-    Object get(Object key) {
-        throw new UnsupportedOperationException();
+    private Entry[] elements;
+
+    private int amountOfElements;
+    private double loadFactor;
+    private int threshold;
+
+    public HashTable (int initialCapacity) {
+        int capacity = getPower2(initialCapacity);
+        elements = new Entry[capacity];
+        amountOfElements = 0;
+        loadFactor = DEFAULT_LOAD_FACTOR;
+        threshold = (int) Math.round(capacity * DEFAULT_LOAD_FACTOR);
+    }
+    public HashTable (int initialCapacity, double newLoadFactor) {
+        int capacity = getPower2(initialCapacity);
+        elements = new Entry[capacity];
+        amountOfElements = 0;
+        loadFactor = newLoadFactor;
+        threshold = (int) Math.round(capacity * DEFAULT_LOAD_FACTOR);
     }
 
-    Object remove(Object key) {
-        throw new UnsupportedOperationException();
+    private int getPower2(int value) {
+        int now = 1;
+        while (now < value) {
+            now = now * 2;
+        }
+        return now;
     }
 
-    int size() {
-        throw new UnsupportedOperationException();
+    private void arrayExpansion() {
+        Entry[] oldElements = elements;
+        elements = new Entry[2 * elements.length];
+        amountOfElements = 0;
+        threshold = (int) Math.round(elements.length * loadFactor);
+        for (Entry oldElement : oldElements) {
+            if (!(oldElement == null || oldElement.tombstone)) {
+                put(oldElement.key, oldElement.value);
+            }
+        }
     }
 
+    private int increaseIndex(int index) {
+        return (index + 1) % elements.length;
+    }
+
+    private int getHash(Object object) {
+        return Math.abs(object.hashCode()) % elements.length;
+    }
+
+    private void moveCluster(int index) {
+        Entry now = elements[index];
+        while (!(elements[increaseIndex(index)] == null || elements[increaseIndex(index)].tombstone)) {
+            Entry next = elements[increaseIndex(index)];
+            elements[increaseIndex(index)] = now;
+            elements[increaseIndex(index)].positionInArray++;
+            now = next;
+            index = increaseIndex(index);
+        }
+        elements[increaseIndex(index)] = now;
+        elements[increaseIndex(index)].positionInArray++;
+    }
+
+    public Object put(Object key, Object value) {
+        int actualPos = getHash(key);
+        int deviationInPosition = 0;
+        for (int i = 0; i < threshold; i++) {
+            // check for free cell
+            if (elements[actualPos] == null) {
+                elements[actualPos] = new Entry(key, value, deviationInPosition);
+                amountOfElements++;
+                if (amountOfElements >= threshold) {
+                    arrayExpansion();
+                }
+                return null;
+            }
+            // check for tombstone
+            if (elements[actualPos].tombstone) {
+                Object anotherKey = elements[actualPos].key;
+                // check for tombstone with equals keys
+                if (key.equals(anotherKey)) {
+                    elements[actualPos] = new Entry(key, value, deviationInPosition);
+                    amountOfElements++;
+                    if (amountOfElements >= threshold) {
+                        arrayExpansion();
+                    }
+                    return null;
+                }
+            }
+            //check for equal keys
+            Object anotherKey = elements[actualPos].key;
+            if (key.equals(anotherKey)) {
+                Object prevValue = elements[actualPos].value;
+                elements[actualPos].value = value;
+                return prevValue;
+            }
+            // check difference in position
+            if (deviationInPosition > elements[actualPos].positionInArray) {
+                moveCluster(actualPos);
+                elements[actualPos] = new Entry(key, value, deviationInPosition);
+                amountOfElements++;
+                if (amountOfElements >= threshold) {
+                    arrayExpansion();
+                }
+                return null;
+            }
+            actualPos = increaseIndex(actualPos);
+            deviationInPosition++;
+        }
+        return null;
+    }
+
+    public Object get(Object key) {
+        int actualPos = getHash(key);
+        int deviationInPosition = 0;
+        for (int i = 0; i < threshold; i++) {
+            // check for null
+            if (elements[actualPos] == null) {
+                return null;
+            }
+            //check position
+            if (deviationInPosition > elements[actualPos].positionInArray) {
+                return null;
+            }
+            // check for equals keys
+            Object anotherKey = elements[actualPos].key;
+            if (key.equals(anotherKey) && !elements[actualPos].tombstone) {
+                return elements[actualPos].value;
+            }
+            actualPos = increaseIndex(actualPos);
+            deviationInPosition++;
+        }
+        return null;
+    }
+
+    public Object remove(Object key) {
+        int actualPos = getHash(key);
+        int deviationInPosition = 0;
+        for (int i = 0; i < threshold; i++) {
+            // check for null
+            if (elements[actualPos] == null) {
+                return null;
+            }
+            // check position
+            if (deviationInPosition > elements[actualPos].positionInArray) {
+                return null;
+            }
+            // check for equals keys
+            Object anotherKey = elements[actualPos].key;
+            if (key.equals(anotherKey) && !elements[actualPos].tombstone) {
+                elements[actualPos].tombstone = true;
+                amountOfElements--;
+                return elements[actualPos].value;
+            }
+            actualPos = increaseIndex(actualPos);
+            deviationInPosition++;
+        }
+        return null;
+    }
+
+    public int size() {
+        return amountOfElements;
+    }
 }
